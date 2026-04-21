e3986040fb7b50e59fa0cbc1ca48f1d193f21c1e
Gilenisson Santos <gilenisson@icloud.com>
2026-04-20 22:00:54 -0300

    Create irigacao-esp32.ino

---
+++ irigacao-esp32.ino
@@ -0,0 +1,74 @@
+
+Plantação de Cana Copy
+Docs
+
+123456789101112131415161718192021222324252627282930
+#include <DHT.h>
+
+#define DHTPIN 15
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+#define BTN_N 2
+#define BTN_P 4
+#define BTN_K 5
+
+Simulation
+00:12.896
+75%
+
+
+
+DHT22
+PWR LED VCC GND DO AO DO LED
+ets Jul 29 2019 12:21:46
+
+rst:0x1 (POWERON_RESET),boot:0x13 (SPI_FAST_FLASH_BOOT)
+configsip: 0, SPIWP:0xee
+clk_drv:0x00,q_drv:0x00,d_drv:0x00,cs0_drv:0x00,hd_drv:0x00,wp_drv:0x00
+mode:DIO, clock div:2
+load:0x3fff0030,len:1156
+load:0x40078000,len:11456
+ho 0 tail 12 room 4
+load:0x40080400,len:2972
+entry 0x400805dc
+------
+Umidade: 40.00
+pH: 3.42
+NPK ativos: 0
+Irrigação DESLIGADA
+------
+Umidade: 40.00
+pH: 3.42
+NPK ativos: 0
+Irrigação DESLIGADA
+------
+Umidade: 40.00
+pH: 3.42
+NPK ativos: 0
+Irrigação DESLIGADA
+------
+Umidade: 40.00
+pH: 3.42
+NPK ativos: 0
+Irrigação DESLIGADA
+------
+Umidade: 40.00
+pH: 3.42
+NPK ativos: 0
+Irrigação DESLIGADA
+------
+Umidade: 40.00
+pH: 3.42
+NPK ativos: 0
+Irrigação DESLIGADA
+------
+Umidade: 40.00
+pH: 3.42
+NPK ativos: 0
+Irrigação DESLIGADA
+
+
+
+
